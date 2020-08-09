eaf928368a0a58236fd01484f1fbb55ccb42b7aa
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-09 06:52:34 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,8 +2,8 @@ def using_until
   levitation_force = 6
   #your code here
 until levitation_force == 10
-  puts "The current number is less than 20."
-  += 1
+  puts ""
+  levitation_force += 1
 end
     
 end
